10697e62dbfa8c408a4e43470ef28d9cb2efb91f
lalagias <jimkountanis@gmail.com>
2019-11-24 19:46:05 +0200

    New directories and mvp architecture

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,4 @@
-// Global app controller
-import num from "./test";
-const x = 23;
+import str from "./models/Search";
+import { add as a, multiply as m, ID } from "./views/searchView";
 
-console.log(
-  `I imported ${num} from another module called test.js! Variable x  is ${x}`
-);
+console.log(`Using imported functions! ${a(ID, 2)} and ${m(3, 5)}. ${str}`);
