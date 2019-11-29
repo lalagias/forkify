0e2f2d015fa76cfe6cf4ff3570f9ea402885fd9a
lalagias <jimkountanis@gmail.com>
2019-11-29 21:47:38 +0200

    added recipe model and controller

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,5 @@
 import Search from "./models/Search";
+import Recipe from "./models/Recipe";
 import * as searchView from "./views/searchView";
 import { elements, renderLoader, clearLoader } from "./views/base";
 
@@ -23,12 +24,17 @@ const controlSearch = async () => {
     searchView.clearResults();
     renderLoader(elements.searchRes);
 
-    // 4) Search for recipes
-    await state.search.getResults();
+    try {
+      // 4) Search for recipes
+      await state.search.getResults();
 
-    // 5) render results on UI
-    clearLoader();
-    searchView.renderResults(state.search.result);
+      // 5) render results on UI
+      clearLoader();
+      searchView.renderResults(state.search.result);
+    } catch (error) {
+      alert("Something wrong with the search...");
+      clearLoader;
+    }
   }
 };
 
@@ -45,4 +51,40 @@ elements.searchResPages.addEventListener("click", e => {
     searchView.clearResults();
     searchView.renderResults(state.search.result, goToPage);
   }
+});
+
+/**
+ * RECIPE CONTROLLER
+ */
+const controlRecipe = async () => {
+  // Get ID from url
+  const id = window.location.hash.replace("#", "");
+  console.log(id);
+
+  if (id) {
+    // Prepare UI for changes
+
+    // Create new recipe object
+    state.recipe = new Recipe(id);
+
+    try {
+      // Get recipe data
+      await state.recipe.getRecipe();
+      state.recipe.parseIngredients();
+
+      // Calculate servings and time
+      state.recipe.calcTime();
+      state.recipe.calcServings();
+
+      // Render recipe
+      console.log(state.recipe);
+    } catch (error) {
+      alert("Error processing recipe!");
+    }
+  }
+};
+
+["hashChange", "load"].forEach(event => {
+  console.log("hashChanged");
+  window.addEventListener(event, controlRecipe);
 });
