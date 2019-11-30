ef343ea37d8f66391e3dc5c2456317d5aba3761f
lalagias <jimkountanis@gmail.com>
2019-11-30 23:15:55 +0200

    added list, shopping and like MVC

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,11 @@
 import Search from "./models/Search";
 import Recipe from "./models/Recipe";
+import List from "./models/List";
+import Likes from "./models/Likes";
 import * as searchView from "./views/searchView";
+import * as recipeView from "./views/recipeView";
+import * as listView from "./views/listView";
+import * as likesView from "./views/likesView";
 import { elements, renderLoader, clearLoader } from "./views/base";
 
 /** GLOBAL STATE OF THE APP
@@ -11,10 +16,13 @@ import { elements, renderLoader, clearLoader } from "./views/base";
  */
 const state = {};
 
+/**
+ * SEARCH CONTROLLER
+ */
 const controlSearch = async () => {
   // 1) Get query from view
   const query = searchView.getInput();
-  console.log(query);
+
   if (query) {
     // 2) New search object and add to state
     state.search = new Search(query);
@@ -59,10 +67,14 @@ elements.searchResPages.addEventListener("click", e => {
 const controlRecipe = async () => {
   // Get ID from url
   const id = window.location.hash.replace("#", "");
-  console.log(id);
 
   if (id) {
     // Prepare UI for changes
+    recipeView.clearRecipe();
+    renderLoader(elements.recipe);
+
+    //Hightlight selected search item
+    if (state.search) searchView.highlightSelected(id);
 
     // Create new recipe object
     state.recipe = new Recipe(id);
@@ -77,14 +89,125 @@ const controlRecipe = async () => {
       state.recipe.calcServings();
 
       // Render recipe
-      console.log(state.recipe);
-    } catch (error) {
+      clearLoader();
+      recipeView.renderRecipe(state.recipe, state.likes.isLiked(id));
+    } catch (err) {
+      console.log(err);
       alert("Error processing recipe!");
     }
   }
 };
 
-["hashChange", "load"].forEach(event => {
-  console.log("hashChanged");
-  window.addEventListener(event, controlRecipe);
+["hashchange", "load"].forEach(event =>
+  window.addEventListener(event, controlRecipe)
+);
+
+/**
+ * LIST CONTROLLER
+ */
+
+const controlList = () => {
+  // Create a new list IF there is none yet
+  if (!state.list) state.list = new List();
+
+  // Add each ingredient to the list
+  state.recipe.ingredients.forEach(el => {
+    const item = state.list.addItem(el.count, el.unit, el.ingredient);
+    listView.renderItem(item);
+  });
+};
+
+// Handle delete and update list item events
+elements.shopping.addEventListener("click", e => {
+  const id = e.target.closest(".shopping__item").dataset.itemid;
+
+  // Handle the delete button
+  if (e.target.matches(".shopping__delete, .shopping__delete *")) {
+    // Delete from state
+    state.list.deleteItem(id);
+
+    // Delete from UI
+    listView.deleteItem(id);
+  } else if (
+    e.target.matches(".shopping__count-value, .shopping__count-value *")
+  ) {
+    //Handle the count update
+    const val = parseFloat(e.target.value, 10);
+    state.list.updateCount(id, val);
+  }
 });
+
+/**
+ * LIKE CONTROLLER
+ */
+
+const controlLike = () => {
+  if (!state.likes) state.likes = new Likes();
+  const currentID = state.recipe.id;
+
+  // User has NOT yet liked current recipe
+  if (!state.likes.isLiked(currentID)) {
+    // Add like to the state
+    const newLike = state.likes.addLike(
+      currentID,
+      state.recipe.title,
+      state.recipe.author,
+      state.recipe.img
+    );
+
+    // Toggle the like button
+    likesView.toggleLikeBtn(true);
+
+    // Add Like to UI list
+    likesView.renderLike(newLike);
+    // User HAS liked current recipe
+  } else {
+    // Remove like to the state
+    state.likes.deleteLike(currentID);
+
+    // Toggle the like button
+    likesView.toggleLikeBtn(false);
+
+    // Remove Like to UI list
+    likesView.deleteLike(currentID);
+  }
+
+  likesView.toggleLikeMenu(state.likes.getNumLikes());
+};
+
+// Restore liked recipes on page load
+window.addEventListener("load", () => {
+  state.likes = new Likes();
+
+  // Restore likes
+  state.likes.readStorage();
+
+  // Toggle like menu button
+  likesView.toggleLikeMenu(state.likes.getNumLikes());
+
+  // Render the existing likes
+  state.likes.likes.forEach(like => likesView.renderLike(like));
+});
+
+// Handling recipe button clicks
+elements.recipe.addEventListener("click", e => {
+  if (e.target.matches(".btn-decrease, .btn-decrease *")) {
+    // Decrease button is clicked
+    if (state.recipe.servings > 1) {
+      state.recipe.updateServings("dec");
+      recipeView.updateServingsIngredients(state.recipe);
+    }
+  } else if (e.target.matches(".btn-increase, .btn-increase *")) {
+    // Increase button is clicked
+    state.recipe.updateServings("inc");
+    recipeView.updateServingsIngredients(state.recipe);
+  } else if (e.target.matches(".recipe__btn--add, .recipe__btn--add *")) {
+    // Add ingredients to shopping list
+    controlList();
+  } else if (e.target.matches(".recipe__love, .recipe__love *")) {
+    // Like controller
+    controlLike();
+  }
+});
+
+window.l = new List();
--- src/js/models/Recipe.js
+++ src/js/models/Recipe.js
@@ -54,6 +54,8 @@ export default class Recipe {
       "pound"
     ];
 
+    const units = [...unitsShort, "kg", "g"];
+
     const newIngredients = this.ingredients.map(el => {
       // 1) Uniform units
       let ingredient = el.toLowerCase();
@@ -66,7 +68,7 @@ export default class Recipe {
 
       // 3) Parse ingredients into count, unit and ingredient
       const arrIng = ingredient.split(" ");
-      const unitIndex = arrIng.findIndex(el2 => unitsShort.includes(el2));
+      const unitIndex = arrIng.findIndex(el2 => units.includes(el2));
 
       let objIng;
       if (unitIndex > -1) {
@@ -104,5 +106,17 @@ export default class Recipe {
       return objIng;
     });
     this.ingredients = newIngredients;
+  }
+
+  updateServings(type) {
+    // Servings
+    const newServings = type === "dec" ? this.servings - 1 : this.servings + 1;
+
+    // Ingredients
+    this.ingredients.forEach(ing => {
+      ing.count *= newServings / this.servings;
+    });
+
+    this.servings = newServings;
   }
 }
